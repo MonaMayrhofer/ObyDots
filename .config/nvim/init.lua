dad4c93a6e3c45660e74ec405c5aace46a07287f
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2021-06-15 19:44:35 +0200

    Some Rust-Analyzer SyntaxCompletion fun

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,8 +8,7 @@ local opt = vim.opt  -- to set options
 cmd 'packadd paq-nvim'               -- load the package manager
 local paq = require('paq-nvim').paq  -- a convenient alias
 paq {'savq/paq-nvim', opt = true}    -- paq-nvim manages itself
-paq {'shougo/deoplete-lsp'}
-paq {'shougo/deoplete.nvim', run = fn['remote#host#UpdateRemotePlugins']}
+paq {'hrsh7th/nvim-compe'}
 paq {'nvim-treesitter/nvim-treesitter'}
 paq {'neovim/nvim-lspconfig'}
 paq {'junegunn/fzf', run = fn['fzf#install']}
@@ -17,16 +16,73 @@ paq {'junegunn/fzf.vim'}
 paq {'ojroques/nvim-lspfuzzy'}
 g['deoplete#enable_at_startup'] = 1  -- enable deoplete at startup
 
--- nvim_lsp object
-local nvim_lsp = require('lspconfig')
+--- SETUP THESE PLUGINS YEE---
+require('lspfuzzy').setup{}
 
--- function to attach completion when setting up lsp
--- local on_attach = function(client)
--- require'completion'.on_attach(client)
--- end
+require('.lsp')
+--------- COPIED STUFF --------
+-- COMPE FROM https://github.com/neovim/nvim-lspconfig/wiki/Autocompletion
+require'compe'.setup {
+  enabled = true;
+  autocomplete = true;
+  debug = false;
+  min_length = 1;
+  preselect = 'enable';
+  throttle_time = 80;
+  source_timeout = 200;
+  incomplete_delay = 400;
+  max_abbr_width = 100;
+  max_kind_width = 100;
+  max_menu_width = 100;
+  documentation = true;
 
--- Enable rust_analyzer
-nvim_lsp.rust_analyzer.setup({ on_attach=on_attach })
+  source = {
+    path = true;
+    nvim_lsp = true;
+  };
+}
+
+local t = function(str)
+  return vim.api.nvim_replace_termcodes(str, true, true, true)
+end
+
+local check_back_space = function()
+    local col = vim.fn.col('.') - 1
+    if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+        return true
+    else
+        return false
+    end
+end
+
+-- Use (s-)tab to:
+--- move to prev/next item in completion menuone
+--- jump to prev/next snippet's placeholder
+_G.tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-n>"
+  elseif check_back_space() then
+    return t "<Tab>"
+  else
+    return vim.fn['compe#complete']()
+  end
+end
+_G.s_tab_complete = function()
+  if vim.fn.pumvisible() == 1 then
+    return t "<C-p>"
+  else
+    return t "<S-Tab>"
+  end
+end
+
+vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+
+
+
+
 
 -- Enable diagnostics
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
