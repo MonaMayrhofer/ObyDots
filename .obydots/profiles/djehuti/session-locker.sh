4716a86f6aa6373364a7070304edb306e2851a45
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2021-09-06 18:48:33 +0200

    Configurable lock

---
+++ .obydots/profiles/djehuti/session-locker.sh
@@ -0,0 +1,3 @@
+start-locker() {
+	betterlockscreen -l blur -- --nofork -k --inside-color=9d59d2 --ring-color=f9f5f0 --insidever-color=9d59d2 --ringver-color=7d3ab1 --insidewrong-color=9d59d2 --ringwrong-color=ed7e7a --line-color=7d3ab1 --keyhl-color=7d3ab1 --bshl-color=ddd60e --greeter-color=ffffff --layout-color=f9f5f0 --time-color=f9f5f0 --date-color=f9f5f0 --verif-color=f9f5f0 --wrong-color=f9f5f0 --modif-color=f9f5f0 --verif-text="" --wrong-text="" --time-font="JetBrainsMono NF" --date-font="JetBrainsMono NF" --timeoutline-width=20
+}
