93d0e0e200c4d503e7be55968599a35952afd52f
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2021-10-08 18:35:47 +0200

    some stuff

--- .config/nvim/lua/lsp.lua
+++ .config/nvim/lua/lsp.lua
@@ -64,7 +64,7 @@ end
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { "pyright", "bashls", "tsserver", "gdscript", "hls", "angularls" }
+local servers = { "pyright", "bashls", "tsserver", "gdscript", "hls", "angularls", "texlab" }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup { on_attach = on_attach }
 end
