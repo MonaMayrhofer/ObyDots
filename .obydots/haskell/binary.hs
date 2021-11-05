6d369608f1e92cd75c875be9dffe93d2f91fdb96
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2021-11-05 08:24:45 +0100

    stuff

--- .obydots/haskell/binary.hs
+++ .obydots/haskell/binary.hs
@@ -1,4 +1,16 @@
-tobin :: Integral a => a -> [a]
-tobin 0 = [0]
-tobin n = (tobin (div n 2)) ++ [(mod n 2)]
+import Data.Char
+
+tobins :: Integral a => a -> [a]
+tobins 0 = [0]
+tobins n = (tobins (div n 2)) ++ [(mod n 2)]
+
+frombins :: [Char] -> Int
+frombins [] = 0
+frombins (x:xs) = (digitToInt x)*(2^(length xs)) + frombins xs
+
+tobinkk :: (Num a, Integral b) => a -> [b]
+tobinkk n = if n >= 0 
+              then tobins (toInt n)
+              else tobinkk (-n)
+
 
