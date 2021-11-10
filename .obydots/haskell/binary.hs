fb8f6cf5bca0d8f0af0d3b45ac1e2745ca4cb312
Erik Mayrhofer <erik.mayrhofer@liwest.at>
2021-11-10 16:48:49 +0100

    haskell binary

--- .obydots/haskell/binary.hs
+++ .obydots/haskell/binary.hs
@@ -1,16 +1,16 @@
 import Data.Char
 
-tobins :: Integral a => a -> [a]
-tobins 0 = [0]
-tobins n = (tobins (div n 2)) ++ [(mod n 2)]
+tobins :: Integral a => a -> [Bool]
+tobins 0 = [False]
+tobins n = (tobins (div n 2)) ++ [(1 == mod n 2)]
 
 frombins :: [Char] -> Int
 frombins [] = 0
 frombins (x:xs) = (digitToInt x)*(2^(length xs)) + frombins xs
 
-tobinkk :: (Num a, Integral b) => a -> [b]
+tobinkk :: (Integral a) => a -> [Bool]
 tobinkk n = if n >= 0 
-              then tobins (toInt n)
-              else tobinkk (-n)
+              then tobins n
+              else map not $ tobinkk $ negate n - 1
 
 
