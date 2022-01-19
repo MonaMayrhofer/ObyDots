c9494bd1ffc381f875b4649df90459d31f94df3d
Mona Mayrhofer <nionidh@gmail.com>
2022-01-19 01:01:15 +0100

    background and lockscreen

--- .obydots/profiles/djehuti/background-profiles.sh
+++ .obydots/profiles/djehuti/background-profiles.sh
@@ -1,7 +1,7 @@
 setroot-backgrounds() {
-	setroot -z "${IMAGE_ROOT}/background.png" -z "${IMAGE_ROOT}/background.png" --blank-color "#191919"
+	setroot -z "${IMAGE_ROOT}/background.png" -z "${IMAGE_ROOT}/background.png" --blank-color "#1f212a"
 }
 
 setroot-blank() {
-	setroot -sc "#191919" -sc "#191919" --blank-color "#191919"
+	setroot -sc "#1f212a" -sc "#1f212a" --blank-color "#1f212a"
 }
--- .obydots/profiles/djehuti/session-locker.sh
+++ .obydots/profiles/djehuti/session-locker.sh
@@ -1,3 +1,24 @@
 start-locker() {
-	betterlockscreen -l blur -- --nofork -k --inside-color=9d59d2 --ring-color=f9f5f0 --insidever-color=9d59d2 --ringver-color=7d3ab1 --insidewrong-color=9d59d2 --ringwrong-color=ed7e7a --line-color=7d3ab1 --keyhl-color=7d3ab1 --bshl-color=ddd60e --greeter-color=ffffff --layout-color=f9f5f0 --time-color=f9f5f0 --date-color=f9f5f0 --verif-color=f9f5f0 --wrong-color=f9f5f0 --modif-color=f9f5f0 --verif-text="" --wrong-text="" --time-font="JetBrainsMono NF" --date-font="JetBrainsMono NF" --timeoutline-width=20
+	betterlockscreen -l blur -- --nofork -k \
+    --inside-color=282a36 \
+    --ring-color=282a36 \
+    --ringver-color=44475a \
+    --insidever-color=282a36 \
+    --ringwrong-color=ff5555 \
+    --insidewrong-color=282a36 \
+    --line-color=FFFFFF \
+    --keyhl-color=44475a \
+    --bshl-color=ff5555 \
+    --greeter-color=f8f8f8 \
+    --layout-color=f8f8f8 \
+    --time-color=f8f8f8 \
+    --date-color=f8f8f8 \
+    --verif-color=f8f8f8 \
+    --wrong-color=f8f8f8 \
+    --modif-color=f8f8f8 \
+    --verif-text="" \
+    --wrong-text="" \
+    --time-font="JetBrainsMono NF" \
+    --date-font="JetBrainsMono NF" \
+    --timeoutline-width=20
 }
